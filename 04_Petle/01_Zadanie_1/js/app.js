ec8648b11046b86aeb55c40797fb792897e7ba23
Julian Mierzejewski <j3mierz@gmail.com>
2024-05-04 10:23:24 +0200

    petle

---
+++ 04_Petle/01_Zadanie_1/js/app.js
@@ -0,0 +1,7 @@
+let y = 8
+let result = 1
+for (let x = 1; x <= y; x++){
+	result = result * x
+
+}
+console.log(result)
---
+++ 04_Petle/02_Zadanie_2/js/app.js
@@ -0,0 +1,7 @@
+let x = 5;
+let y = 10;
+let sum = 0;
+for (let i = x; i <= y; i++) {
+	sum += i;
+}
+console.log(sum);
