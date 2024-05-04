0a1df77647f79b45d8ed29008f6475eabed646a9
Julian Mierzejewski <j3mierz@gmail.com>
2024-05-04 10:07:20 +0200

    instrukcje warunkowe

---
+++ 03_Instrukcje_warunkowe/01_Zadanie_1/js/app.js
@@ -0,0 +1,5 @@
+const age = prompt("ile masz lat")
+
+let aget = age < 18 ? "niepełnoletni" : "pełnoletni"
+console.log(aget);
+
---
+++ 03_Instrukcje_warunkowe/02_Zadanie_2/js/app.js
@@ -0,0 +1,30 @@
+const grade = null
+switch(grade) {
+	case 6 : {
+	console.log("celujący");
+	break;
+	}
+	case 5 : {
+	console.log("bdb");
+	break;
+	}
+	case 4 : {
+	console.log("db");
+	break;
+	}
+	case 3 : {
+	console.log("dst");
+	break;
+	}
+	case 2 : {
+	console.log("dop");
+	break;
+	}
+	case 1 : {
+	console.log("niedostateczny");
+	break;
+	}	
+	default : {
+	console.log("brak oceny")
+	}
+}
